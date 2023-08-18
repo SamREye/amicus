d0f4e876645501fe7d80976fa9979c678ff82ec5
Samuel Bourque <sam.bourque@gmail.com>
2023-08-18 17:22:34 -0300

    OpenAI key

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import github
 
 # Get Access Token for GitHub
 gh_token = os.environ.get('GITHUB_TOKEN')
+openai_apikey = os.environ.get('OPENAI_API_KEY')
 
 # Create GitHub object
 gh = github.GitHub(gh_token)
