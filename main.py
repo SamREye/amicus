1bf56d81f437ae255efda4d8697866ae52d21c47
Samuel Bourque <sam.bourque@gmail.com>
2023-08-18 18:00:05 -0300

    Query enabled repos and list PRs

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os, sys
+import uuid
 
 from lib.GitHubWrapper import GithubWrapper
 
@@ -19,9 +20,36 @@ def main():
     # Get the list of repos for each owner
     repos = []
     for owner in owners:
-        for repo in gh.get_repos(owner):
+        for repo in gh.get_repos(owner, visibility="public"):
             repos.append(repo)
-    print(repos)
+
+    activated_repos = []
+    # Check to see if the repo has a AMICUS.md file
+    for repo in repos:
+        # Get the AMICUS.md file
+        try:
+            file = repo.get_contents("AMICUS.json")
+            activated_repos.append({
+                "repo": repo,
+                "amicus_cfg": file.decoded_content.decode("utf-8")
+            })
+        except:
+            # print("No AMICUS.md file found")
+            continue
+    
+    # Get all pull requests for each repo
+    prs = []
+    for repo in activated_repos:
+        # Get all pull requests
+        for pr in repo["repo"].get_pulls():
+            # Get the pull request's hash
+            pr_hash = pr.head.sha
+            prs.append({
+                "hash": pr_hash,
+                "uuid": uuid.uuid4(),
+            })
+    
+    print(prs)
 
 if __name__ == '__main__':
     main()
