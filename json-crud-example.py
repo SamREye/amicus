f6b8310a52b19b415803699b72d43a2f0f80fa09
Waldoh <neo-c4@protonmail.com>
2023-08-19 03:23:44 -0300

    added data for getting repo name and owner

--- json-crud-example.py
+++ json-crud-example.py
@@ -1,15 +1,28 @@
 import uuid
 from lib.crud import CRUD
 
-database_name = "queue.json"
-
-crud = CRUD(database_name)
+DATABASE_NAME = "queue.json"
+crud = CRUD(DATABASE_NAME)
 
 session_id = str(uuid.uuid4())
 
-pull_requests = [{"hash": "39ee68c87bb0018c5ca31c00b1ecf1d5f688faec"}]
+# I need the pull request ID from the repo
 
-crud.add_pull_requests_to_queue(session_id, pull_requests)
+# I need the owner + repo name
+repo_owner = "SamREye"
+repo_name = "amicus"
+
+pull_requests = [{"id": "1"}, {"id": "2"}]
+
+# write to json file
+crud.add_pull_requests_to_queue(session_id, pull_requests, repo_name, repo_owner)
+
+# update status
 crud.update_status(session_id, "done")
+
+# get data by session id
 print(crud.get_data_by_session_id(session_id))
+
+# delete by session id
 # crud.delete_by_session_id(session_id)
+
